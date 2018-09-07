7393472c2db19fb84162fd503e82536ac90fcf7a
Jennifer <32855060+jennnico@users.noreply.github.com>
2018-09-07 15:20:10 -0400

    Legend added

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-//Resource: https://bl.ocks.org/mbostock/4122298
+//https://bl.ocks.org/mbostock/4122298
 
-
-var w = 960;
-var h = 600;
-  
+document.addEventListener('DOMContentLoaded',function(){
+  req=new XMLHttpRequest();
+  req.open("GET",'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json',true);
+  req.send();
+  req.onload=function(){
+    //Education attainment data
+    json=JSON.parse(req.responseText);
+   console.log(json[1])
+   
 var svg = d3.select("svg");
 
 var path = d3.geoPath();
@@ -11,20 +16,64 @@ var path = d3.geoPath();
 d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
   if (error) throw error;
 
-  svg.append("g")
-      .attr("class", "counties")
-   //   .attr("class", "states")
-      .selectAll("path")
-      .data(topojson.feature(us, us.objects.counties).features)
- // .data(topojson.feature(us, us.objects.states).features)
-      .enter().append("path")
-      .attr("d", path);
+ //Make the counties
+ svg.append("g")
+    .attr("class", "counties")
+    .selectAll("path")
+    .data(topojson.feature(us, us.objects.counties).features)
+    .enter().append("path")
+    .attr("d", path);
 
+ //Make the county borders
  svg.append("path")
-      .attr("class", "county-borders")
-      .attr("d", path(topojson.mesh(us, us.objects.counties, function(a, b) { return a !== b; })));
+    .attr("class", "county-borders")
+    .attr("d", path(topojson.mesh(us, us.objects.counties, function(a, b) { return a !== b; })));
 
+  //Make the state borders
   svg.append("path")
       .attr("class", "state-borders")
       .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
+  
+  //Legend data (percentages)
+  const legend = [25, 50, 75, 100]
+  
+  //Add the legend canvas.
+  const svgLegend=d3.select("#legend")
+                      .append("svg")
+                      .attr("width", 960)
+                      .attr("height", 40)
+  
+  //Add the legend rectangles.
+  svgLegend.selectAll("rect")
+           .data(legend)
+           .enter()
+           .append("rect")
+           .attr("width", 40)
+           .attr("height", 10)
+           .attr("x", (d, i) => {return 650 + i*40})
+           .attr("y", 1)
+           .style('fill', (d) => {
+              if (d <= 25){ //2.8, navy
+                return ("rgba(12, 81, 0, .3)")
+              }else if (d <= 50){
+                return("rgba(12, 81, 0, .5)")
+              }else if (d <= 75){
+                return("rgba(12, 81, 0, .7)")
+              }else{
+                return("rgba(12, 81, 0, 1)")
+              }
+           })
+  
+  //Add the legend text
+  svgLegend.selectAll("text")
+           .data(legend)
+           .enter()
+           .append("text")
+           .attr("x", (d, i) => 680 + i*40 )
+           .attr("y", (d, i) => 30)
+           .text(d=>d + "%")
+  
+  
 });
+    
+      }})
